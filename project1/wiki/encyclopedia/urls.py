e986a59f181f7f4f80d573707fda23a21849dfcf
ArthurSOA <sodrearthur2@gmail.com>
2022-09-21 04:58:45 -0300

    Projeto Finalizado

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("wiki/<str:entry>",views.entry,name="entry"),
     path("newEntry/",views.newEntry,name="newEntry"),
     path("random/",views.random,name="random"),
-    path("search/",views.search,name="search")
+    path("search/",views.search,name="search"),
+    path("wiki/<str:entry>/edit/",views.edit,name = "edit")
     ]
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -9,12 +9,17 @@ from django.urls import reverse
 from django import forms
 from random import choice
 from . import util
+#from wiki import encyclopedia
+
 
 class FormAddEntry(forms.Form):
-    title = forms.CharField(label = "Entry Title")
-    body = forms.CharField(widget = forms.Textarea)
-    edit = forms.BooleanField(initial = False, widget=forms.HiddenInput, required= False)
+    title = forms.CharField(label = "Entry Title",widget = forms.TextInput(attrs = {'class': 'form-control col-md-8 col-lg-8'}))
+    body = forms.CharField(widget = forms.Textarea(attrs = {'class': 'form-control col-md-8 col-lg-8', 'rows': 10}))
+    edit = forms.BooleanField(initial = False,widget=forms.HiddenInput(),required=False)
 
+
+
+  
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -63,5 +68,43 @@ def random(request):
 
 def newEntry(request):
     if request.method =="GET":
-        return render(request, "encyclopedia/newPage.html")
+        return render(request, "encyclopedia/newEntry.html", {
+            "form": FormAddEntry(),
+            "exists": False,
+            "editing": False
+        }
+        )
+
+    elif request.method =="POST":
+        form = FormAddEntry(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            edit = form.cleaned_data["edit"]
+
+
+        if (util.get_entry(title) is not None) and (edit == False):
+             return render(request, "encyclopedia/newEntry.html", {
+                    "form": FormAddEntry(),
+                    "exists": True,
+                    "editing": False
+                    })
+        else:
+            util.save_entry(title,body)
+            return HttpResponseRedirect(reverse("entry",kwargs={'entry': title}))
+
+def edit(request,entry):
+
+    form=FormAddEntry()
+    form.fields["title"].initial = entry
+    form.fields["title"].widget = forms.HiddenInput()
+    form.fields["body"].initial = util.get_entry(entry)
+    form.fields["edit"].initial = True
+    
+
+    return render(request, "encyclopedia/newEntry.html", {
+        "form": form,
+        "exists": False,
+        "editing": True
+        })
 
